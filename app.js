504697255a52144e12917c0790ee06b9ca8d36e5
Sergey <caandspb@gmail.com>
2018-05-17 02:17:38 +0300

    add user registration in db

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const passport = require('passport');
+const mongoose = require('mongoose');
+const config = require('./config/database');
+
+//Connection to database
+mongoose.connect(config.database);
+
+//On connection
+mongoose.connection.on('connected', () => {
+    console.log('Connected to database ' + config.database);
+});
+
+//On error
+mongoose.connection.on('error', (err) => {
+    console.log('Database error: ' + err);
+});
+
+const app = express();
+
+const users = require('./routes/users');
+
+const port = 3000;
+
+//CORS Middleware
+app.use(cors());
+
+//Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Body parser Middleware
+app.use(bodyParser.json());
+
+app.use('/users', users);
+
+app.get('/', (req, res) => {
+    res.send('Invalid Endpoin');
+});
+
+app.listen(port, () => {
+    console.log('Server started on port ' + port);
+});
